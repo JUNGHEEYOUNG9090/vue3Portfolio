3eb9801c85dc21df94851eba91120ed80ae83e15
jung <jung@118.32.177.250>
2024-11-22 16:27:19 +0900

    목록 수정

--- vue3TestProject/src/main/java/com/vue3Test/demo/repository/PostsRepository.java
+++ vue3TestProject/src/main/java/com/vue3Test/demo/repository/PostsRepository.java
@@ -13,10 +13,15 @@ import com.vue3Test.demo.model.PostsModel;
 @Repository
 public interface PostsRepository extends JpaRepository<PostsModel, Long> {
     
-	@Query(value = "SELECT d.id, d.title, d.update_dt, 'devLog' AS type FROM dev_log d " +
-            "UNION ALL " +
-            "SELECT image_id as id, i.image_src AS title, i.update_dt, 'image' AS type FROM imagelistinfo i " +
-            "ORDER BY d.update_dt DESC. id desc",
+	@Query(value = "SELECT id, title, update_dt, type "
+			+ "FROM ("
+			+ "    SELECT d.id, d.title, d.update_dt, 'devLog' AS type "
+			+ "    FROM dev_log d "
+			+ "    UNION ALL "
+			+ "    SELECT image_id as id, i.image_src AS title, i.update_dt, 'image' AS type "
+			+ "    FROM imagelistinfo i"
+			+ ") as union_table "
+			+ "ORDER BY update_dt DESC, id DESC ",
     countQuery = "SELECT (SELECT COUNT(*) FROM dev_log) + (SELECT COUNT(*) FROM imagelistinfo)",
     nativeQuery = true)
 	Page<Object[]> findAllPosts(Pageable pageable);
